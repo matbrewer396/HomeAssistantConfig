ce8a5c0fbd44903ba7b1a6da188fe48851623690
Matt_Brewer <matbrewer396@gmail.com>
2019-12-03 17:43:06 +0000

    Bath room window

--- GetLastConfig.bat
+++ GetLastConfig.bat
@@ -1,2 +1,3 @@
-robocopy y:\ "C:\Users\matt\Documents\HomeAssistantConfig" /s /e /XF home-assistant_v2.db
+robocopy y:\ "C:\Users\matt\Documents\HomeAssistantConfig" /s /e /XF home-assistant_v2.db /XF home-assistant_v2.db /xd deps-old
+del "C:\Users\matt\Documents\HomeAssistantConfig\flows.json" >nul 2>&1
 C:\Users\matt\Desktop\jq-win64.exe . C:\Users\matt\Documents\HomeAssistantConfig\node-red\flows.json >> C:\Users\matt\Documents\HomeAssistantConfig\flows.json
